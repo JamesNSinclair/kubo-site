486eb5180318d07abdba50ada3cfc23b9a665e4a
JamesNSinclair <53437429+JamesNSinclair@users.noreply.github.com>
2020-06-07 09:08:17 +0100

    Aloado f changed

--- javascript/main-scripts.js
+++ javascript/main-scripts.js
@@ -3,8 +3,12 @@ const info = document.getElementById("home-content")
 
 
     vid.addEventListener('ended',makeVisible,false);
+
     function makeVisible(e) {
-        console.log("done!");
-        info.classList.remove('hide');
-        info.classList.add('fade-in');
-    }
+
+          window.setTimeout(()=>{
+             info.classList.remove('hide');
+           info.classList.add('fade-in');
+         }, 500)
+
+       }
